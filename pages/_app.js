bef12bd4048564e4fb0c1005d968bbf700694227
Sarthak Mohanty <sarthak@sarthakmohanty.me>
2024-05-25 17:38:52 -0400

    add speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { Provider } from "react-wrap-balancer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Provider>
         <Component {...pageProps} />
+        <SpeedInsights />
       </Provider>
     </ThemeProvider>
   );
