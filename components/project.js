5e0d8167715fa574b23b57c0ae97fd086365bf78
Sarthak Mohanty <sarthak@sarthakmohanty.me>
2024-05-25 18:04:52 -0400

    i can't remember what i changed

--- components/project.js
+++ components/project.js
@@ -9,6 +9,7 @@ export default function Project({
   return (
     <div className="flex flex-col items-start">
       <img
+        id={`image-for-${title}`}
         className="max-h-32 w-auto object-contain hover:active:scale-150 origin-bottom-left md:origin-center md:hover:scale-[300%] transition duration-200 ease-in-out cursor-pointer mb-2"
         style={{
           "-webkit-touch-callout":
