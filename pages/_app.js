884bedf858c3c5339c08f4c3e5fceeb2b8614ed1
Sarthak Mohanty <sarthak@sarthakmohanty.me>
2024-05-25 10:16:01 -0400

    refactor extra boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import '../styles/globals.css'
-import { ThemeProvider } from 'next-themes'
-import { Provider } from 'react-wrap-balancer'
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { Provider } from "react-wrap-balancer";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="system" attribute="class">
+    <ThemeProvider defaultTheme="light" attribute="class">
       <Provider>
         <Component {...pageProps} />
       </Provider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
